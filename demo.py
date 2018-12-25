95ad0aa88a8ae11d6b5ee8262d66339d8b92943f
oyiadin <oyiadin@gmail.com>
2018-12-25 11:07:20 +0800

    gen demo video

--- demo.py
+++ demo.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import sys
 import os.path
 import time
 from six.moves import xrange  # pylint: disable=redefined-builtin
@@ -26,6 +28,7 @@ import tensorflow as tf
 import input_data
 import c3d_model
 import numpy as np
+import random
 import cv2
 from PIL import Image
 
@@ -36,6 +39,11 @@ flags = tf.app.flags
 gpu_num = 2
 flags.DEFINE_integer('batch_size', 10 , 'Batch size.')
 FLAGS = flags.FLAGS
+
+model_name = "./sports1m_finetuning_ucf101.model"
+crop_size = 112
+np_mean = np.load('crop_mean.npy').reshape([16, crop_size, crop_size, 3])
+
 
 def placeholder_inputs(batch_size):
   """Generate placeholder variables to represent the input tensors.
@@ -71,12 +79,9 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
     tf.add_to_collection('losses', weight_decay)
   return var
 
-def demo_main_loop(cap):
-  model_name = "./sports1m_finetuning_ucf101.model"
-  crop_size = 112
-  np_mean = np.load('crop_mean.npy').reshape([16, crop_size, crop_size, 3])
 
-  # Get the sets of images and labels for training, validation, and
+def init_ops():
+    # Get the sets of images and labels for training, validation, and
   images_placeholder, labels_placeholder = placeholder_inputs(FLAGS.batch_size * gpu_num)
   with tf.variable_scope('var_name') as var_scope:
     weights = {
@@ -119,12 +124,18 @@ def demo_main_loop(cap):
   # Create a saver for writing training checkpoints.
   saver.restore(sess, model_name)
   # And then after everything is built, start the training loop.
+  return sess, images_placeholder, norm_score
+
+
+def demo_main_loop(cap):
+  sess, X, norm_score = init_ops()
 
   #############################################################################
   #############################################################################
   #############################################################################
 
   # main loop
+  top1_predicted_label = 'None'
   while True:
     img_datas = []
 
@@ -146,7 +157,10 @@ def demo_main_loop(cap):
         img = img[crop_x:crop_x+crop_size, crop_y:crop_y+crop_size, :] - np_mean[len(img_datas)]
         img_datas.append(img)
 
-      cv2.imshow('frame', cv2.resize(frame, (256, 144)))
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      cv2.putText(frame, top1_predicted_label, (10, 100), font, 3, (255,255,255),5, cv2.LINE_AA, bottomLeftOrigin=False)
+
+      cv2.imshow('frame', cv2.resize(frame, (384, 216)))
       if cv2.waitKey(15) & 0xFF == ord('q'):
         print('goodbye')
         return
@@ -154,7 +168,7 @@ def demo_main_loop(cap):
     img_datas = np.array(img_datas)
     # to label the last 16 frames captured by opencv2 using our model
     zeros = np.tile(np.zeros_like(img_datas[np.newaxis, :]).T, 20).T
-    print(zeros.shape, np.array(img_datas).shape)
+    # print(zeros.shape, np.array(img_datas).shape)
     test_images = zeros
     test_images[0] = img_datas
     # convert to (20, 16, 112, 112, 3)
@@ -162,20 +176,108 @@ def demo_main_loop(cap):
     start_time = time.time()
     score = norm_score.eval(
             session=sess,
-            feed_dict={images_placeholder: test_images}
+            feed_dict={X: test_images}
             )[0]
-    top1_predicted_label = np.argmax(score)
-    print(labels[top1_predicted_label])
-    print(' (It took %ds to get this result...)' % int(time.time() - start_time))
+    top1_predicted_label = labels[np.argmax(score)]
+    print('the top 1 predicted label is:', top1_predicted_label)
+    print('  (It took %ds to get this result...)\n' % int(time.time() - start_time))
+
+
+def gen_list(inf, outf):
+  with open(inf) as f:
+    f = list(f)
+    selected_lines = random.sample(f, 20)
+    with open(outf, 'w') as ff:
+      ff.write(''.join(selected_lines))
+      ff.close()
+  print('list generated')
+
+
+def gen_demo_video(inf, outf):
+  print('loading list file..')
+  lines = [i.strip().split() for i in list(open(inf))]
+  assert len(lines) == 20
+  dirs = [i[0] for i in lines]
+  true_labels = [i[1] for i in lines]
+
+  fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+  out = cv2.VideoWriter(outf, fourcc, 16.0, (512, 288))
+
+  print('init and restore the model..')
+  sess, X, norm_score = init_ops()
+  all_data = []
+  all_original = []
+  for i in range(len(dirs)):
+    print('preprocessing', dirs[i])
+    preprocessed_frames = []
+    original_frames = input_data.get_frames_data(dirs[i])[0][:16]
+    for frame in original_frames:
+      # preprocessing the frames so that it can suits for our pretrained model
+      img = Image.fromarray(frame.astype(np.uint8))
+      # assert img.width > img.height:
+      scale = float(crop_size) / float(img.height)
+      img = np.array(cv2.resize(np.array(img), (int(img.width * scale + 1), crop_size))).astype(np.float32)
+      crop_x = int((img.shape[0] - crop_size)/2)
+      crop_y = int((img.shape[1] - crop_size)/2)
+      img = img[crop_x:crop_x+crop_size, crop_y:crop_y+crop_size, :] - np_mean[len(preprocessed_frames)]
+      preprocessed_frames.append(img)
+    # print(np.array(preprocessed_frames).shape)
+    all_data.append(np.array(preprocessed_frames))
+    all_original.append(np.array(original_frames))
+  # print(np.array(all_data).shape)
+
+  print('all data have already been preprocessed')
+  print('predicting..')
+  start_time = time.time()
+  scores = norm_score.eval(
+    session=sess,
+    feed_dict={X: np.array(all_data)}
+  )
+  pred_labels = []
+  for i in scores:
+    pred_labels.append(np.argmax(i))
+    print('{:>30} : true_label = {} ; pred_label = {}'.format(
+      dirs[len(pred_labels)-1].split(os.sep)[-1],
+      true_labels[len(pred_labels)-1],
+      pred_labels[-1]))
+  print('predicting took me {}s'.format(int(time.time() - start_time)))
+
+  print('saving to video...')
+  for n, video in enumerate(all_original):
+    print('saving: [ {:0>2} / {:0>2} ]'.format(n+1, len(all_original)))
+    for frame in video:
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      color = (0, 255, 127) if pred_labels[n] == true_labels[n] else (255, 0, 0)
+      cv2.putText(frame, labels[pred_labels[n]], (10, 40),
+        font, 1, color, 2, cv2.LINE_AA, bottomLeftOrigin=False)
+      resized = cv2.resize(frame, (512, 288))
+      out.write(resized)
+  
+  print('saved, goodbye!')
+  out.release()
+
 
 def main(_):
-  #try:
-  cap = cv2.VideoCapture(0)
-  demo_main_loop(cap)
-  #except KeyboardInterrupt:
-  #  print('goodbye')
-  cap.release()
-  cv2.destroyAllWindows()
+  if len(sys.argv) >= 2:
+    if sys.argv[1].lower() == 'gen_list':
+      gen_list(sys.argv[2], sys.argv[3])
+    elif sys.argv[1].lower() == 'demo_video':
+      gen_demo_video(sys.argv[2], sys.argv[3])
+    elif sys.argv[1].lower() == 'help':
+      print('* python3 demo.py gen_list in_file out_file')
+      print('* python3 demo.py demo_video in_file out_file')
+      print('* python3 demo.py')
+      print('  (this will capture images from your camera)')
+    else:
+      print('unknown option, run `python3 demo.py help`')
+  else:
+    try:
+      cap = cv2.VideoCapture(0)
+      demo_main_loop(cap)
+    except KeyboardInterrupt:
+      print('goodbye')
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
   tf.app.run()
